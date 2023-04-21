14796bd056deb9b6135836a7e012c8c3eb0d26a4
Mik-Halo <sprone3@gmail.com>
2023-04-21 20:32:12 +0300

    home page added

---
+++ src/components/DriverStandingsTab.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaChevronRight } from "react-icons/fa";
+
+export default function DriverStandingsTab({ results }) {
+  const { StandingsTable } = results;
+  const { StandingsLists } = StandingsTable || {};
+  const { DriverStandings } = StandingsLists?.[0] || {};
+
+  return (
+    <div className="tab-results-table">
+      <h2>Driver Standings</h2>
+      {DriverStandings &&
+      <div className="tab-podium">
+        <Link to={`/drivers/${DriverStandings[1].Driver.driverId}`}>
+          <div className="tab-podium-card">
+            <div style={{ color: `var(--${DriverStandings[1].Constructors[0].constructorId}_color)`}} className="tab-podium-avatar">
+              <h3>2</h3>
+              <img src={`/images/drivers/${DriverStandings[1].Driver.code}-thumbnail.png`} 
+              alt={DriverStandings[1].Driver.driverId} 
+              />
+            </div>
+            <div className="tab-podium-name">
+              <p>{DriverStandings[1].Driver.givenName}</p>
+              <p>{DriverStandings[1].Driver.familyName}</p>
+            </div>
+          </div>
+        </Link>
+
+        <Link to={`/drivers/${DriverStandings[0].Driver.driverId}`}>
+          <div className="tab-podium-card winner-card">
+            <div style={{ color: `var(--${DriverStandings[0].Constructors[0].constructorId}_color)`}} className="tab-podium-avatar">
+              <h3>1</h3>
+              <img src={`/images/drivers/${DriverStandings[0].Driver.code}-thumbnail.png`} 
+              alt={DriverStandings[0].Driver.driverId} 
+              />
+            </div>
+            <div className="tab-podium-name">
+              <p>{DriverStandings[0].Driver.givenName}</p>
+              <p>{DriverStandings[0].Driver.familyName}</p>
+            </div>
+          </div>
+        </Link>
+
+        <Link to={`/drivers/${DriverStandings[2].Driver.driverId}`}>
+          <div className="tab-podium-card">
+            <div style={{ color: `var(--${DriverStandings[2].Constructors[0].constructorId}_color)`}} className="tab-podium-avatar">
+              <h3>3</h3>
+              <img src={`/images/drivers/${DriverStandings[2].Driver.code}-thumbnail.png`} 
+              alt={DriverStandings[2].Driver.driverId} 
+              />
+            </div>
+            <div className="tab-podium-name">
+              <p>{DriverStandings[2].Driver.givenName}</p>
+              <p>{DriverStandings[2].Driver.familyName}</p>
+            </div>
+          </div>
+        </Link>
+      </div>
+      }
+      {DriverStandings?.map(item => {
+        return (
+        <Link to={`/drivers/${item.Driver.driverId}`}>
+          <div className="tab-results-row" key={item.position}>
+            <p style={{ borderColor: `var(--${item.Constructors[0].constructorId}_color)`}}>{item.position}</p>
+            <p>{item.Driver.givenName} <strong>{item.Driver.familyName}</strong> <span className="tab-row-team">{item.Constructors[0].name}</span></p>
+            <p>{item.points} PTS</p>
+            <FaChevronRight />
+          </div>
+        </Link>)
+      })
+      }
+      {/* {results.StandingsTable && results.StandingsTable.StandingsLists[0].DriverStandings.map(item => {
+        return (
+        <div className="tab-results-row" key={item.position}>
+          <p>{item.position}</p>
+          <p>{item.Driver.givenName} {item.Driver.familyName}</p>
+          <p>{item.points} PTS</p>
+        </div>)
+      })
+      } */}
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,139 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
+import DriverStandingsTab from '../components/DriverStandingsTab';
 import './Home.css';
 
+const DRIVERS_URL = 'https://ergast.com/api/f1/current/driverStandings.json?limit=10';
+const CONSTRUCTORS_URL = 'https://ergast.com/api/f1/current/constructorStandings.json';
+const LAST_RACE_URL = 'https://ergast.com/api/f1/current/last/results.json?limit=10';
+
 export default function Home() {
-  return (
-    <div className="container-sm">
-      <div className="parallax-container">
-        <div className="main-news">
-          <div>
-            <h2>Wolff says W14 pace is 'only the tip of the iceberg' as Mercedes upgrades imminent</h2>
-          </div>
-          <div className="static-img">
-          </div>
-        </div>
-        <div className="scrollable-imgs">
-          <img src="/images/drivers/BOT.jpg" alt="" />
-          <img src="/images/drivers/DEV.jpg" alt="" />
-        </div>
+  const [loading, setLoading] = useState(false);
+  const [selectedTab, setSelectedTab] = useState('drivers');
+  const [results, setResults] = useState([]);
+
+  async function fetchResults(url) {
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      let results = await response.json();
+      results = results.MRData;
+      setResults(results);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  function handleTabClick(e) {
+    if (e.currentTarget.value === 'drivers') {
+      fetchResults(DRIVERS_URL);
+      setSelectedTab(e.currentTarget.value);
+    } else if (e.currentTarget.value === 'constructors') {
+      fetchResults(CONSTRUCTORS_URL);
+      setSelectedTab(e.currentTarget.value);
+    } else if (e.currentTarget.value === 'last-race') {
+      fetchResults(LAST_RACE_URL);
+      setSelectedTab(e.currentTarget.value);
+    }
+  }
+
+  useEffect(() => {
+    fetchResults(DRIVERS_URL);
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <Loading />
       </div>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae temporibus voluptatibus a, omnis ipsum cum, dicta consectetur provident distinctio enim in aut itaque dolores ut quas necessitatibus veniam reprehenderit sunt? Reiciendis magni, molestias nihil vel nobis ullam illo harum saepe similique? Autem natus error earum fugit aliquam, minus quo porro.</div>
-    </div>
+    )
+  }
+
+  return (
+    <>
+      <div className="container-sm">
+        <div className="news-container">
+          <div className="main-news">
+            <span className="news-tag">news</span>
+            <h2>Wolff says W14 pace is 'only the tip of the iceberg' as Mercedes upgrades imminent</h2>
+            <div className="main-news-thumbnail">
+              <img src="/images/news/news_thumbnail.png" alt="" />
+            </div>
+          </div>
+
+          <div className="other-news">
+            <div className="news-div">
+              <div className="news-thumbnail">
+                <img src="/images/news/news1_thumbnail.png" alt="" />
+              </div>
+              <div className="news-heading">
+                <span className="news-tag">news</span>
+                <p>McLaren form new driver development programme led by ex-driver Emanuele Pirro</p>
+              </div>
+            </div>
+            <div className="news-div">
+              <div className="news-thumbnail">
+                <img src="/images/news/news1_thumbnail.png" alt="" />
+              </div>
+              <div className="news-heading">
+                <span className="news-tag">news</span>
+                <p>McLaren form new driver development programme led by ex-driver Emanuele Pirro</p>
+              </div>
+            </div>
+            <div className="news-div">
+              <div className="news-thumbnail">
+                <img src="/images/news/news1_thumbnail.png" alt="" />
+              </div>
+              <div className="news-heading">
+                <span className="news-tag">news</span>
+                <p>McLaren form new driver development programme led by ex-driver Emanuele Pirro</p>
+              </div>
+            </div>
+            <div className="news-div">
+              <div className="news-thumbnail">
+                <img src="/images/news/news1_thumbnail.png" alt="" />
+              </div>
+              <div className="news-heading">
+                <span className="news-tag">news</span>
+                <p>McLaren form new driver development programme led by ex-driver Emanuele Pirro</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className="tab-list">
+        <button className={`tab ${selectedTab === 'drivers' ? 'active-tab': ''}`} value="drivers" onClick={handleTabClick}><p>Drivers</p></button>
+        <button className={`tab ${selectedTab === 'constructors' ? 'active-tab': ''}`} value="constructors" onClick={handleTabClick}><p>Constructors</p></button>
+        <button className={`tab ${selectedTab === 'last-race' ? 'active-tab': ''}`} value="last-race" onClick={handleTabClick}><p>Last Race</p></button>
+      </div>
+      <div className="tab-results">
+        {selectedTab === 'drivers' && <DriverStandingsTab results={results} />
+        }
+        {selectedTab === 'constructors' && results.StandingsTable 
+        && results.StandingsTable.StandingsLists[0].ConstructorStandings.map(item => {
+          return (
+          <div className="tab-results-row" key={item.position}>
+            <p>{item.position}</p>
+            <p>{item.Constructor.name}</p>
+            <p>{item.points}</p>
+          </div>)
+        })
+        }
+        {selectedTab === 'last-race' && results.RaceTable 
+        && results.RaceTable.Races[0].Results.map(item => {
+          return (
+          <div className="tab-results-row" key={item.position}>
+            <p>{item.position}</p>
+            <p>{item.Driver.givenName} {item.Driver.familyName}</p>
+            <p>{item.points}</p>
+          </div>)
+        })
+        }
+      </div>
+    </>
   )
 }
